f78ee693a4b6a2f00c0c85101d8a51372cd6646b
Aitor Pascual <apascual053@ikasle.ehu.eus>
2024-03-22 20:26:56 +0100

    Monitor: Hasta el ejercicio 5

--- Monitor3/client.c
+++ Monitor3/client.c
@@ -17,17 +17,23 @@ typedef struct {
 	int secret2;
 } Secrets;
 
+union semun {
+	int val;
+	struct semid_ds *buf;
+	unsigned short *array;
+};
+
 int menuser();
 void ejercicio1(Secrets *secrets);
 void ejercicio2(Secrets *secrets);
-void ejercicio3(int *sem_id);
-void ejercicio4(int *sem_id);
+void ejercicio3();
+void ejercicio4();
 void ejercicio5();
 void ejercicio6();
 void ejercicio7();
 
 int main(int argc, char** argv) {
-        int opt, shm_id, sem_id;
+        int opt;
 	Secrets secrets;
 
         do{
@@ -44,10 +50,10 @@ int main(int argc, char** argv) {
                                 ejercicio2(&secrets);
                                 break;
                         case 3:
-                                ejercicio3(&sem_id);
+                                ejercicio3();
                                 break;
                         case 4:
-                                ejercicio4(&sem_id);
+                                ejercicio4();
                                 break;
                         case 5:
 				ejercicio5();
@@ -182,26 +188,22 @@ void ejercicio2(Secrets *secrets)
 	printf("Ejercicio 2: desvinculado de shm.\n");
 }
 
-void ejercicio3(int *sem_id)
+void ejercicio3()
 {
+	int sem_id;
 	int key_sem = 476;
 
 	// Crear el semáforo
-	if ((*sem_id = semget(KEY, 1, 0666 | IPC_CREAT)) == -1)
+	if ((sem_id = semget(KEY, 1, 0666 | IPC_CREAT)) == -1)
 	{
 		perror("Ejercicio3: sgmet error.\n");
 		exit(-1);
 	}
 
 	// Inicializar el semáforo
-	union semun {
-		int val;
-		struct semid_ds *buf;
-		unsigned short *array;
-	} arg;
-
+	union semun arg;
 	arg.val = key_sem;
-	if (semctl(*sem_id, 0, SETVAL, arg) == -1)
+	if (semctl(sem_id, 0, SETVAL, arg) == -1)
 	{
 		perror("Ejercico3: error semctl.\n");
 	       exit(-1);
@@ -210,13 +212,14 @@ void ejercicio3(int *sem_id)
 	printf("Semáforo creado e inicializado con valor %d\n", key_sem);	
 }
 
-void ejercicio4(int *sem_id)
+void ejercicio4()
 {
+	int sem_id;
 	int sem_val[2] = {1,2};
 	struct sembuf sem_opc[2];
 
 	// Crear el array de semáforos
-	if ((*sem_id = semget(KEY, 2, 0666 | IPC_CREAT)) == -1)
+	if ((sem_id = semget(KEY, 2, 0666 | IPC_CREAT)) == -1)
 	{
 		perror("Ejercicio4: error semget.\n");
 		exit(-1);
@@ -225,14 +228,9 @@ void ejercicio4(int *sem_id)
 	// Inicializar los semáforos
 	for (int i = 0; i < 2; i++)
 	{
-		union semun {
-			int val;
-			struct semid_ds *buf;
-			unsigned short *array;
-		} arg;
-
+		union semun arg;
 		arg.val = sem_val[i];
-		if (semctl(*sem_id, i, SETVAL, arg) == -1)
+		if (semctl(sem_id, i, SETVAL, arg) == -1)
 		{
 			perror("Ejercicio4: error semctl.\n");
 			exit(-1);
@@ -241,6 +239,69 @@ void ejercicio4(int *sem_id)
 		printf("Semaforo %d inicializado con valor %d\n", i + 1, sem_val[i]);
 	}
 }
-void ejercicio5(){}
+
+void ejercicio5()
+{
+	// Crear el semáforo
+	int sem_id;
+	if ((sem_id = semget(KEY, 1, 0666 | IPC_CREAT)) == -1)
+	{
+		perror("Ejercicio5: error semget.\n");
+		exit(-1);
+	}
+
+	// Crear el segmento de memoria compartida
+	int shm_id = shmget(KEY, MAX_SIZE, 0666 | IPC_CREAT);
+	if (shm_id == -1)
+	{
+		perror("Ejercicio5: shmget error.\n");
+		exit(-1);
+	}
+
+	// Asociar el segmento de momeria compartida al proceso
+	void *shmaddr = shmat(shm_id, NULL, 0);
+	if (shmaddr == (void *)-1)
+	{
+		perror("Ejercicio5: shmat error.\n");
+		exit(-1);
+	}
+
+	// Sincronización con el semáforo
+	struct sembuf sem_op;
+	sem_op.sem_num = 0;
+	sem_op.sem_op = -1; // esperar a que el semáforo esté en verde
+	sem_op.sem_flg = 0;
+	
+	if (semop(sem_id, &sem_op, 1) == -1)
+	{
+		perror("Ejercicio5: semop 1 error.\n");
+		exit(-1);
+	}
+
+	// Leer y modificar el dato en la memoria compartida
+	int *data = (int *)shmaddr;
+	int clave = *data;
+	*data = -clave; // Cambiar el signo de la clave
+
+	sleep(3);
+
+	// Poner el semaforo en 1, desbloquear el monitor
+	sem_op.sem_op = 1;
+	if (semop(sem_id, &sem_op, 1) == -1)
+	{
+		perror("Ejercicio5: semop 2 error.\n");
+		exit(-1);
+	}
+
+	printf("Clave obtenida %d\n", clave);
+
+	// Desvincular el segmento de memoria compartida
+    	if (shmdt(shmaddr) == -1) 
+	{
+        	perror("shmdt");
+        	exit(EXIT_FAILURE);
+    	}
+}
+
 void ejercicio6(){}
 void ejercicio7(){}
